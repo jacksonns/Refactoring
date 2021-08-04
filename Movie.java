76f00f3c5d6f6d5bed3d901471163ebb7a56ce93
jacksonns <>
2021-08-04 17:23:21 +0000

    Commit 10

--- Movie.java
+++ Movie.java
@@ -41,4 +41,13 @@ public class Movie {
     }
     return thisAmount;    
   }
+
+  public int getFrequentRenterPoints(int daysRented){
+    int frequentRenterPoints = 0;
+    frequentRenterPoints ++;
+    // add bonus for a two day new release rental
+    if ((this.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) frequentRenterPoints ++;
+    return frequentRenterPoints;
+  }
+
 }
